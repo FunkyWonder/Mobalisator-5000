8a6c5ee49be2005dcfc028757d90557f72d911f0
Arne Cuperus <cuperusarne@gmail.com>
2023-05-12 20:55:12 +0200

    Initialize swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
